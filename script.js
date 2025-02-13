ccf80bb11d5fb222d25b0126cce95d0766a578c7
Ibrahim Wafi Asalya <iwa1999995@gmail.com>
2025-02-13 20:35:00 +0200

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,15 @@ document.addEventListener("DOMContentLoaded", function () {
 
     menuToggle.addEventListener("click", function () {
         sidebar.classList.toggle("show");
+           const header = document.querySelector(".header");
+
+    window.addEventListener("scroll", function() {
+        if (window.scrollY > 50) {
+            header.classList.add("scrolled");
+        } else {
+            header.classList.remove("scrolled");
+        }
+    });
     });
 
     // جلب الأخبار الحية
